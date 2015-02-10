5f76e4d51a6bc6290bd32360211495422059c973
BenoitGoimier <benoitgoimier@gmail.com>
2015-02-11 00:47:39 +0100

    Mise en Ligne

---
+++ app/currencyApp.js
@@ -0,0 +1,5 @@
+(function() {
+
+	var currencyApp = angular.module('currencyApp', []);
+
+})();
---
+++ app/currencyController.js
@@ -0,0 +1,31 @@
+(function() {
+	angular.module('currencyApp').controller("CurrencyController", ['$scope', '$http', function ($scope, $http) {
+
+		$scope.what = 1;
+		$scope.from = 'EUR';
+		$scope.to = 'USD';
+		$scope.result = 1.12855;
+
+		$http.get('./app/data/currencymap.json').success (function(data) {
+			$scope.currencies = data;
+		});
+
+		this.getResult = function() {
+			this.url = 'http://rate-exchange.appspot.com/currency?' +
+			'from=' + $scope.from +
+			'&to=' + $scope.to +
+			'&q=' + $scope.what +
+			'&callback=JSON_CALLBACK';
+
+			$http.jsonp(this.url).success(function(data) {
+				$scope.result = data.v;
+			});
+		};
+
+		this.swap = function() {
+			this.tmp = $scope.from;
+			$scope.from = $scope.to;
+			$scope.to = this.tmp;
+		};
+	}]);
+})();
