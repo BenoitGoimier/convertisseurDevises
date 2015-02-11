4369cb71ce373abf4608c76d776c672df7a61ca3
BenoitGoimier <benoitgoimier@gmail.com>
2015-02-11 10:10:43 +0100

    CSS + Js en local

--- app/currencyController.js
+++ app/currencyController.js
@@ -1,31 +1,30 @@
-(function() {
-	angular.module('currencyApp').controller("CurrencyController", ['$scope', '$http', function ($scope, $http) {
+angular.module('currencyApp').controller("CurrencyController", ['$scope', '$http', function ($scope, $http) {
 
-		$scope.what = 1;
-		$scope.from = 'EUR';
-		$scope.to = 'USD';
-		$scope.result = 'Saisir une valeur';
+    $scope.what = 1;
+    $scope.from = 'EUR';
+    $scope.to = 'USD';
+    $scope.result = 'En cours...';
+    $scope.history = [];
 
-		$http.get('./app/data/currencymap.json').success (function(data) {
-			$scope.currencies = data;
-		});
+    $http.get('./app/data/currencymap.json').success (function(data) {
+        $scope.currencies = data;
+    });
 
-		this.getResult = function() {
-			this.url = 'http://rate-exchange.appspot.com/currency?' +
-			'from=' + $scope.from +
-			'&to=' + $scope.to +
-			'&q=' + $scope.what +
-			'&callback=JSON_CALLBACK';
+    this.getResult = function() {
+        this.url = 'http://rate-exchange.appspot.com/currency?' +
+        'from=' + $scope.from +
+        '&to=' + $scope.to +
+        '&q=' + $scope.what +
+        '&callback=JSON_CALLBACK';
 
-			$http.jsonp(this.url).success(function(data) {
-				$scope.result = data.v;
-			});
-		};
+        $http.jsonp(this.url).success(function(data) {
+            $scope.result = data.v;
+        });
+    };
 
-		this.swap = function() {
-			this.tmp = $scope.from;
-			$scope.from = $scope.to;
-			$scope.to = this.tmp;
-		};
-	}]);
-})();
+    this.swap = function() {
+        this.tmp = $scope.from;
+        $scope.from = $scope.to;
+        $scope.to = this.tmp;
+    };
+}]);
